566f8e9b1168bb70016e1417d2bb3b613a6afe36
Cristian-Lopez <126030346+CristianLopez3@users.noreply.github.com>
2023-10-03 18:49:07 -0500

    add catching and alpine version

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.json([{
-  name: 'Cristian',
-  email: 'Cristian@mail.com'
-}]))
+app.get('/', (req, res) => res.json([
+  {
+    name: 'Cristian',
+    email: 'Cristian@mail.com'
+  },
+  {
+    name: 'Alex',
+    email: 'alex@hotmail.com'
+  },
+  {
+    name: 'Maria',
+    email: 'Maria@hotmail.com'
+  }
+]))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
